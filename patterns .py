0bba171a34f672626649717ff20a88406c235452
Burhanuddin-tin24 <tinwalab74@gmail.com>
2023-10-11 16:00:04 +0530

    patters

---
+++ patterns .py
@@ -0,0 +1,25 @@
+n = 5
+for i in range(n,0,-1):
+    for j in range(i):
+     print("*",end="")
+    print()
+  
+for i in range(0,n):
+   for j in range(i+1):
+      print("*",end="")
+   print()
+
+print()
+n = 5
+
+for i in range(0,n):
+   for j in range(i+1):
+      print("*",end="")
+   print()
+
+
+for i in range(n,0,-1):
+    for j in range(i):
+     print("*",end="")
+    print()
+  
