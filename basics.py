76f347a3b39ed7844b9e0f503c8ecf89bb084e1f
Burhanuddin-tin24 <tinwalab74@gmail.com>
2023-10-11 16:22:29 +0530

    basics of python

---
+++ basics.py
@@ -0,0 +1,31 @@
+#DATA TYPES:
+#LIST
+l = [1, 2, 3, 4]
+print(l, type(l))
+print()
+
+#TUPLES
+t = (1, 2, 3, 4)
+print(t, type(t))
+print()
+
+#SETS
+s = {1, 2, 3, 4}
+print(s, type(s))
+print()
+
+#DICTIONARY
+d = {"itachi": "uchiha", "shishui": "uchiha"}
+
+print(d, type(d))
+print()
+
+names = ("itachi", "shishui", "madara", "sasuke" )
+for i in names:
+    print(i)
+print()
+
+for i in range(12):
+ 
+   print("5x", i , "=", 5*i)
+
